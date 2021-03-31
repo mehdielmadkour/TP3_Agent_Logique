653a87d8806338d196b8adfb33935d89eea44059
mehdielmadkour <mehdi.elmadkour@outlook.fr>
2021-03-31 13:15:49 +0200

    first commit

---
+++ TP3_Agent_Logique/TP3_Agent_Logique.cpp
@@ -0,0 +1,12 @@
+﻿// TP3_Agent_Logique.cpp : définit le point d'entrée de l'application.
+//
+
+#include "TP3_Agent_Logique.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
